a54e265efdb291deba4c45f0ec8f2df6fbbe584d
MuskoM <muskomatt@outlook.com>
2021-01-02 19:56:04 +0100

    Changed stuff not relevant

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Movie from '../Movie/Movie';
+
+function App() {
+  return (
+    <div className="MainContainer">
+      <Movie>
+
+      </Movie>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,15 @@
+import { react, Component } from "react";
+
+class Movie extends Component{
+
+    constructor(){
+        super();
+    }
+
+    render(){
+        return(<h1>This is a movie</h1>);
+    }
+
+}
+
+export default Movie;
